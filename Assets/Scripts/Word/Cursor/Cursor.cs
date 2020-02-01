99e98249a89971a5e56f36daed8ed3f7941d41b0
Cominis <like.dmt@gmail.com>
2020-02-01 18:45:46 +0200

    Added smooth adding

--- Assets/Scripts/Word/Cursor/Cursor.cs
+++ Assets/Scripts/Word/Cursor/Cursor.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 public class Cursor : MonoBehaviour
 {
     private Renderer myRenderer;
+    public Transform firstLetter;
     void Start()
     {
+        Debug.Log("yraeeeeeeeee?");
+
+        var firstLetterRenderer = firstLetter.GetComponent<Renderer>();
+        Vector3 position = Vector3.zero;
+        position.x = (firstLetterRenderer.bounds.max.x - firstLetterRenderer.bounds.min.x) / 2;
+        transform.localPosition = position;
+
+
         myRenderer = GetComponent<Renderer>();
         StartCoroutine(Blink());
     }
